20d8976563b45084d25814a5c6224e03650c7476
Andrew <amm@iastate.edu>
2016-07-29 00:28:01 -0500

    Program Works. Need to figure problem with scanner blocking

--- LargestSum/src/Main.java
+++ LargestSum/src/Main.java
@@ -1,10 +1,17 @@
 /**
  * @author Andrew McKeighan
  */
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-	/**
+	
+    
+    public static ArrayList<String> FinalAnswer = new ArrayList<String>();
+    
+    
+    /**
 	 *Given a triangle of positive integers, we want to find a largest possible sum along a path from top
 	 *row to bottom row. A path can go down either directly south or directly south-east (Note, you can
      *NEVER go EAST, WEST, or NORTH etc). For example, in the triangle
@@ -26,13 +33,11 @@ public class Main {
         int numRows = 0;
         String read = "";
         Scanner scan = new Scanner(System.in);
-        while(scan.hasNext()){
+        
+        while(true){
             numRows = Integer.parseInt(scan.next());
             //scan.nextLine();
             makeArray(numRows, scan);
-            if(!scan.hasNext()){
-                break;
-            }
         }
 
 	}
@@ -64,14 +69,46 @@ public class Main {
     public static void findBestPath(int triangle[][], int size){
         int max = 0;
         int maxArr[] = new int[size];
-        
+        int temp[][] = new int[size][];
+        for(int i = 0; i< size; i++){
+            temp[i] = triangle[i].clone();
+        }
+        int maxTri = 0;
+        Stack st = new Stack();
+        Stack fin = new Stack();
         for(int i = size-2; i >= 0; i--){
             for(int j = 0; j <= i; j++){
-                triangle[i][j] += Math.max(triangle[i+1][j], triangle[i+1][j+1]);
+                maxTri = Math.max(triangle[i+1][j], triangle[i+1][j+1]);
+                if(maxTri == triangle[i+1][j]){
+                    st.push(temp[i+1][j]);
+                }else{
+                    st.push(temp[i+1][j+1]);
+                }
+                triangle[i][j] += maxTri;
+                
             }
+            int large = 0;
+            while(!st.isEmpty()){
+                if(large < (int) st.peek()){
+                    large = (int) st.pop();
+                }else{
+                    st.pop();
+                }
+            }
+            fin.push(large);
+            
         }
-        System.out.println(triangle[0][0]);
+        fin.push(temp[0][0]);
+        //System.out.println(triangle[0][0]);
+        FinalAnswer.add("Max is " + triangle [0][0]);
+        String sentence = "";
+        while(!fin.isEmpty()){
+            sentence += fin.pop()+"-->";
+        }
+        sentence = sentence.substring(0, sentence.length()-3);
+        FinalAnswer.add(sentence);
     }
+    
     
     
 }
